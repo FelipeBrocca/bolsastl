08febfa4cac4e49f385cc78b47a889198ba8a939
FelipeBrocca <pipebrocca@gmail.com>
2023-05-22 13:59:41 -0300

    loader

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -44,14 +44,14 @@ const Contact = () => {
     }, []);
 
     return (
-        <div ref={ref} id='contact-section' className='contact-section' 
+        <div ref={ref} id='contact-section' className='contact-section'
         >
             <InfoContact isActive={isActive}>
                 <h2 className='contact-title'>Vías de contacto</h2>
             </InfoContact>
             <div className={`form-container ${isActive ? 'active' : ''}`}>
                 <h2 className={`contact-title ${isActive ? 'active' : ''}`}>Hacenos tu consulta</h2>
-                <form  className='form-contact' ref={form} onSubmit={sendEmail}>
+                <form className='form-contact' ref={form} onSubmit={sendEmail}>
                     <input
                         type='text'
                         placeholder='Nombre completo'
@@ -83,7 +83,11 @@ const Contact = () => {
                             : <button type='submit' className='button-form'>
                                 {
                                     loadingForm
-                                        ? 'Cargando'
+                                        ? <>
+                                            <div className='loader-form-container'>
+                                                <span className="loader-form"></span>
+                                            </div>
+                                        </>
                                         : 'Enviar'
                                 }
                             </button>
